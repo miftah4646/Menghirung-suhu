4d8ef9d1c9ff45f03c70f6b6797d1ce0ac2f55d8
miftah4646 <90054667+miftah4646@users.noreply.github.com>
2021-09-07 18:33:02 +0700

    Add files via upload

---
+++ suhu.py
@@ -0,0 +1,27 @@
+# konversi suhu
+
+
+# program latian konversi celcius ke satuan lain 
+
+
+print("\nPROGRAM KONVERSI CELCIUS\n")
+
+celcius = float (input('Masukkan suhu dalam celcius  : '))
+print("suhu adalah ",celcius, "Celcius")
+
+# reamur
+reamur = (4/5) * celcius
+print("suhu dalam reamur adalah",reamur,"Reamur")
+
+
+
+# fahrenheit
+fahrenheit = ((9/5) * celcius) + 32
+print("suhu dalam fahrenheat adalah",fahrenheit,"Fahrenheid")
+
+
+# kelvin
+kelvin = celcius + 273
+print("suhu dalam kelvin adalah",kelvin,"Kelvin")
+
+
